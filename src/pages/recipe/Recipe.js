00f37af91e3362c52d16b5ab43b66f8042d1ca8d
dkibrahi <dkibrahi@umich.edu>
2022-09-12 10:19:19 -0400

    used useFetch to fetch data for a single recipe and created a recipe form for users to enter custom recipes

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,14 +1,31 @@
 // react imports
 import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
 
 // styles
 import './Recipe.css'
 
 export default function Recipe() {
-  const { info } = useParams();
-  console.log(info);
+  const { id } = useParams();
+  const url = 'http://localhost:3000/recipes/' + id;
+  const {data: recipe, isPending, error }= useFetch(url);
+
+  console.log(recipe);
 
   return (
-    <div>Recipe</div>
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+        <h2 className='page-title'>{recipe.title}</h2>
+        <p>Takes {recipe.cookingTime} to cook</p>
+        <ul>
+          {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+        </ul>
+        <p>{recipe.method}</p>
+        </>
+      )}
+    </div>
   )
 }
