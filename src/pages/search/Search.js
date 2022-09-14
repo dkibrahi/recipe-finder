98168f06f9ddbd8143d5276dd4fbcd17f7b129a9
dkibrahi <dkibrahi@umich.edu>
2022-09-14 14:55:29 -0400

    completed search bar functionality

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,11 +1,29 @@
 // react imports
-import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch';
+
+// components
+import RecipeList from '../../components/RecipeList';
 
 // styles
 import './Search.css'
 
 export default function Search() {
+  const queryStr = useLocation().search;
+
+  const queryParams = new URLSearchParams(queryStr);
+  const query = queryParams.get('q');
+
+  const url = ' http://localhost:3000/recipes?q=' + query;
+  const {data: matchedRecipes, isPending, error } = useFetch(url);
+
+
   return (
-    <div>Search</div>
+    <div>
+      <h2 className='page-title'>Recipes including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {matchedRecipes && <RecipeList recipes={matchedRecipes} />}
+    </div>
   )
 }
